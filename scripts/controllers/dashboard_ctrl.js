8015f22009d70fb23c61a0253ed977251ea6aba7
Uri Brecher <uri.brecher@gmail.com>
2015-10-14 22:46:23 +0300

    1. session_service improved with indexes and promises 2. dashboard is now with basic cancel functionality 3. added some security rules

--- scripts/controllers/dashboard_ctrl.js
+++ scripts/controllers/dashboard_ctrl.js
@@ -3,14 +3,19 @@
 
     angular.module('mainApp').controller('DashboardCtrl', 
                                          ['$scope', '$firebaseObject', '$firebaseArray' ,
-                                          'domain', 'auth_service',
-      function($scope, $firebaseObject, $firebaseArray,domain, auth_service) {
+                                          'domain', 'auth_service','session_service',
+      function($scope, $firebaseObject, $firebaseArray,domain, auth_service, session_service) {
           $scope.auth = auth_service.get_auth_object();
           
           var root_ref = new Firebase(domain);
           var users_ref = root_ref.child("users_public");
           $scope.users = $firebaseArray(users_ref);
           $scope.user = auth_service.get_fb_user();
+          $scope.selection = {
+              session_ids: {
+              }
+          };
+          $scope.select_all;
           
           // TODO: try to avoid this direct usage of auth object
           $scope.auth.$onAuth(function(authData) {
@@ -19,6 +24,27 @@
               }
               var sessions_ref = root_ref.child('sessions').child(authData.uid);
               $scope.sessions = $firebaseArray(sessions_ref);
+              
+              var trainee_sessions_ref = root_ref.child('sessions_index').child('as_trainee').child(authData.uid);
+              
+              trainee_sessions_ref.on('child_added',function(child) {
+                      $scope.selection.session_ids[child.key()] = false;
+              });
           });
+          
+          $scope.check_all = function() {
+              angular.forEach($scope.selection.session_ids, function(selected, session_id) {
+                  $scope.selection.session_ids[session_id] = $scope.select_all;
+              });
+          }
+          
+          $scope.cancel_selected = function() {
+              for (var session_id in $scope.selection.session_ids) {
+                  if ($scope.selection.session_ids[session_id]) {
+                      session_service.cancel_session_request($scope.user.uid, session_id);
+                  }
+              }
+            
+          };
       }]);
 })();
--- scripts/session_service.js
+++ scripts/session_service.js
@@ -32,9 +32,18 @@
             return deferred;
         };
         
+        var get_val_of = function(ref) {
+            var deferred = $q.defer();
+            ref.once("value", function(snap) {
+                deferred.resolve(snap.val());
+            });
+            return deferred.promise;
+        };
+        
         this.send_session_request = function(trainee, trainer, time, workout) {
             var new_session_ref = sessions_ref.child(trainee).child(trainer).push();
-            var trainer_index_ref = sessions_index_ref.child(trainer).child(new_session_ref.key());
+            var trainer_index_ref = sessions_index_ref.child("as_trainer").child(trainer).child(new_session_ref.key());
+            var trainee_index_ref = sessions_index_ref.child("as_trainee").child(trainee).child(new_session_ref.key());
 
             var def1 = q_fb_set(new_session_ref, {
                 time: time,
@@ -43,45 +52,62 @@
             });
             
             var def2 = q_fb_set(trainer_index_ref, trainee);
-            return $q.all([def1,def2]);
+            var def3 = q_fb_set(trainee_index_ref, trainer);
+            return $q.all([def1,def2,def3]);
         };
         
-        this.get_session_reference = function(trainer, session_id) {
-            var trainee_ref = sessions_index_ref.child(trainer).child(session_id);  
-            return sessions_ref.child(trainee_ref.val()).child(trainer).child(session_id);
-        }
+        
+        var get_session_by_trainer = function(trainer_id, session_id) {
+            var trainee_ref = sessions_index_ref.child("as_trainer").child(trainer_id).child(session_id);
+            var promise = get_val_of(trainee_ref).then(function(value) {
+                return sessions_ref.child(value).child(trainer_id).child(session_id);
+            });
+            return promise;
+        };
+        
+        var get_session_by_trainee = function(trainee_id, session_id) {
+            var trainers_ref = sessions_index_ref.child("as_trainee").child(trainee_id).child(session_id);
+            var promise = get_val_of(trainers_ref).then(function(value) {
+                return sessions_ref.child(trainee_id).child(value).child(session_id);
+            });
+            
+            return promise;
+        };
         
         this.confirm_session_request = function(trainer, session_id) {
-            var session_ref = this.get_session_reference(trainer, session_id);
-            return q_fb_transaction(session_ref.child('state'), function(currentState) {
-                if (currentState === "pending") {
-                    return "confirmed";
-                }
-                
-                return;
-            });
+            get_session_by_trainers(trainer, session_id).then(function(session_ref) {
+                return q_fb_transaction(session_ref.child('state'), function(currentState) {
+                    if (currentState === "pending") {
+                        return "confirmed";
+                    }
+
+                    return;
+                });
+            });  
         };
         
         this.reject_session_request = function(trainer, session_id) {
-            var session_ref = this.get_session_reference(trainer, session_id);
-            return q_fb_transaction(session_ref.child('state'), function(currentState) {
-                if (currentState === "pending") {
-                    return "rejected";
-                }
-                
-                return;
-            });
+            get_session_by_trainers(trainer, session_id).then(function(session_ref) {
+                return q_fb_transaction(session_ref.child('state'), function(currentState) {
+                    if (currentState === "pending") {
+                        return "rejected";
+                    }
+
+                    return;
+                });
+            });  
         };
 
-        this.cancel_session_request = function(trainer, session_id) {
-            var session_ref = get_session_reference(trainer, session_id);
-            return q_fb_transaction(session_ref.child('state'), function(currentState) {
-                if (currentState === "pending" || currentState === "confirmed") {
-                    return "cancelled";
-                }
-                
-                return;
-            });
+        this.cancel_session_request = function(trainee, session_id) {
+            get_session_by_trainee(trainee, session_id).then(function(session_ref) {
+                return q_fb_transaction(session_ref.child('state'), function(currentState) {
+                    if (currentState === "pending" || currentState === "confirmed") {
+                        return "cancelled";
+                    }
+
+                    return;
+                });
+            });  
         };
                 
     };
